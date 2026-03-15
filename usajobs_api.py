062f5a9b02f5afce940c65cefab0d614dc74e2d8
Pankti Shaileshkumar Shah <42117506+pankti09@users.noreply.github.com>
2026-03-15 00:47:19 -0400

    Add files via upload

---
+++ usajobs_api.py
@@ -0,0 +1,21 @@
+import requests
+from utils.config import USAJOBS_API_KEY
+
+def fetch_usajobs(keyword, location="remote", results_per_page=5):
+    headers = {
+        "Host": "data.usajobs.gov",
+        "User-Agent": "<Your_Email_Address>",
+        "Authorization-Key": USAJOBS_API_KEY
+    }
+
+    params = {
+        "Keyword": keyword,
+        "LocationName": location,
+        "ResultsPerPage": results_per_page
+    }
+    url = f"https://data.usajobs.gov/api/search?Keyword={keyword}&LocationName={location}&ResultsPerPage={results_per_page}"
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        return response.json().get('SearchResult', {}).get('SearchResultItems', [])
+    else:
+        return []
